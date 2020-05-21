9362343888398c25d66b3de8dfc1f85ae1f248d2
mhkmcp <mhkmcp@yahoo.com>
2020-05-21 22:55:55 +0600

    Added more Test function

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-
 
 from .models import Question, Choice
 
@@ -12,7 +12,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 # def index(request):
@@ -23,6 +25,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     model = Question
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 # def detail(request, question_id):
